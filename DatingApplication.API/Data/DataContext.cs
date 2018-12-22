21c8337c1d3daed9da736785bbbecbeb2ff281bf
Konrad <szkonrad1.2@gmail.com>
2018-12-22 20:30:33 +0100

    Initial Commit

---
+++ DatingApplication.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApplication.API.Models;
+namespace DatingApplication.API.Data
+{
+    public class DataContext:DbContext
+    {
+        
+        public DataContext(DbContextOptions<DataContext> options):base
+        (options)
+        {
+            
+        }
+        public DbSet<Value> Values { get; set; }
+
+        
+    }
+}
